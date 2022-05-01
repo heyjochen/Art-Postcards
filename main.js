8685e4e4149bbd0b18e329bd519526e4bb8f7fc4
heyjochen <jochen@stierberger.com>
2022-05-01 10:29:20 -0500

    add displayImage function

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ function fetchPostcard(ID, number){
             const postcard = new Postcard(artwork)
             postcard.displayArtworkInfo();
             postcard.setImageAlt();
-            // call the postcard functions
+            postcard.displayImage();
 
         })
 
@@ -64,11 +64,16 @@ class Postcard {
         this.department = artworkInfo.data.department_title
         this.artist = artworkInfo.data.artist_titles
         this.style = artworkInfo.data.style_titles
+        this.iiifURL = artworkInfo.config["iiif_url"]
+        this.imageID = artworkInfo.data["image_id"]
 
     }
 
     displayImage(){
-        //
+        // use iiifURL, imageID and /full/843,/0/default.jpg to generate ImageUrl
+        const imagePath = '/full/843,/0/default.jpg'
+        const imageURL = `${this.iiifURL}/${this.imageID}${imagePath}`
+        document.querySelector('#postcard-image').src = imageURL
     }
 
     displayArtworkInfo(){
